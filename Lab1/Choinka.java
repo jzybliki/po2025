7b39d568c3facd643943549c2390d449daebf6ab
Jakub <jakub.zyblikiewicz@gmail.com>
2025-10-06 19:38:37 +0200

    Zajecia 1: Hello i Choinka

---
+++ Lab1/Choinka.java
@@ -0,0 +1,13 @@
+public class Choinka {
+	public static void main(String[] args) {
+		int licznik = 0;
+		integer.pareint = k;
+		while(licznik<k){
+			for(int i=1; i<licznik; i++){
+				System.out.print("*");
+			}
+			System.out.println(" ");
+			licznik++;
+		}
+	}
+}
